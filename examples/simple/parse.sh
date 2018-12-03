b3b2bfcb595ec43ebad1b1862f8f22177f53b756
Bruce Herr II <bherr@indiana.edu>
2018-12-03 09:13:22 -0500

    Updated script to keep the output dir in git, added a requirements.txt

--- examples/simple/parse.sh
+++ examples/simple/parse.sh
@@ -3,6 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 cd $DIR
 
-rm -f output/*
+rm -f output/*; touch output/.gitkeep
 
 ../../generic_parser.py -c config.xml -p People -r Person -i Emp_Id -t template.sql -l file_numbers.csv -d data -o output
